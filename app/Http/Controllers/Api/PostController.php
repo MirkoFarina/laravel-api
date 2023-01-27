f125c7936da5930aec2107272f17a63b7efdd44d
MirkoFarina <MirkoFarina@outlook.com>
2023-01-27 18:32:15 +0100

    [add] control on image in api get details

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -18,6 +18,13 @@ class PostController extends Controller
     public function show($slug)
     {
         $project = Project::where('slug', $slug)->with('type', 'technologies')->first();
+
+        if($project->cover_image){
+            $project->cover_image = url("storage/" . $project->cover_image);
+        }else {
+            $project->cover_image = url("storage/uploads/placeholder.png");
+        }
+
         return response()->json($project);
     }
 }
